fbd253f4b184006ab499cc0d2ec8c8829f11e873
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-06-01 18:08:41 +1000

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("Connecting to purenet servers...")
-x=document.createElement("source");x.src="https://github.com/CdDw-HQ/purenet"; document.head.appendChild(x);
-console.log("Connected. purenet v0.2 beta")
-
