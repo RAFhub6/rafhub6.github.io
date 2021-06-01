d9d50e820a6c76e7c2e4372b2d5643898d338d6e
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-06-01 18:09:09 +1000

    Create index.js

---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+console.log("Connecting to purenet servers...")
+x=document.createElement("source");x.src="https://github.com/CdDw-HQ/purenet"; document.head.appendChild(x);
+console.log("Connected. purenet v0.2 beta")
